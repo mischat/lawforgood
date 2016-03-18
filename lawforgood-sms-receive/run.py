42894b636178e5d0a74110366520413ae932e8ce
Mischa <mischa@mmt.me.uk>
2016-03-18 23:36:45 +0000

    Grab the data accordingly

--- lawforgood-sms-receive/run.py
+++ lawforgood-sms-receive/run.py
@@ -5,16 +5,27 @@ import twilio.twiml
  
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/sms/reply', methods=['GET', 'POST'])
 def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    print(str(request.Body()))
+           
+    resp = twilio.twiml.Response()
+    resp.message('Hello, Mobile Monkey')
+    return str(resp)
+
+@app.route('/voice/reply', methods=['GET', 'POST'])
+def voice_hello_monkey():
     """Respond to incoming calls with a simple text message."""
            
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message('Hello, Voice Monkey')
     return str(resp)
+
                     
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
 
 
 # vi:set expandtab sts=4 sw=4:
