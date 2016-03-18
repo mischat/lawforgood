a85944aca6c8e69a9129aecd77e1f1c4095bf8e7
Mischa Tuffield <mischa@mmt.me.uk>
2016-03-18 22:35:29 +0000

    Intital commit of the sms receiver

---
+++ lawforgood-sms-receive/README.txt
@@ -0,0 +1,22 @@
+## Local development
+
+### Setup
+
+Requirements: python, pip, and virtualenv
+
+Install virtualenv:
+```sh
+pip install virtualenv
+```
+
+Set up the virtualenv 
+```sh
+virtualenv venv
+```
+
+Activate the virtualenv & install the app's dependencies:
+```sh
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
---
+++ lawforgood-sms-receive/run.py
@@ -0,0 +1,20 @@
+#!venv/bin/python
+
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+           
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+                    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# vi:set expandtab sts=4 sw=4:
