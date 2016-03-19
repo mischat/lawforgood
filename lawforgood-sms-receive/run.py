336c99dbc5dd96faa05c4564204cf6aeaa83ced7
Mischa Tuffield <mischa@mmt.me.uk>
2016-03-19 00:31:48 +0000

    To JSON of the text message ..

--- lawforgood-sms-receive/run.py
+++ lawforgood-sms-receive/run.py
@@ -2,18 +2,30 @@
 
 from flask import Flask, request, redirect
 import twilio.twiml
+import datetime
+import json
  
 app = Flask(__name__)
+
 
 @app.route('/sms/reply', methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
-    print(str(request.Body()))
-           
+    sms_body = str(request.args['Body'])
+    sms_from = str(request.args['From'])
+    sms_time = str(datetime.datetime.utcnow().isoformat())
+
+    data = {'from': sms_from, 'body': sms_body, 'time': sms_time}
+    json_data = json.dumps(data)
+
+    print 'JSON: ' + json_data
+
+    print('Message "' + sms_body + '" from " ' + sms_from + ' ' + sms_time)
     resp = twilio.twiml.Response()
     resp.message('Hello, Mobile Monkey')
     return str(resp)
+
 
 @app.route('/voice/reply', methods=['GET', 'POST'])
 def voice_hello_monkey():
