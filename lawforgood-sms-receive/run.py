184fc47c0073ace971fe084b56c176c8d1717308
Mischa Tuffield <mischa@mmt.me.uk>
2016-03-19 14:35:03 +0000

    Fixed port

--- lawforgood-sms-receive/run.py
+++ lawforgood-sms-receive/run.py
@@ -172,7 +172,7 @@ def handle_wave():
     return str('lame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
 
 
 # vi:set expandtab sts=4 sw=4:
